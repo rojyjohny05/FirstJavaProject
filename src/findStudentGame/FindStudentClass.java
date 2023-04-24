0bd2c393c7aae4e3cb156fa70824b97ac38e79a0
rojyjohny05 <rojyjohny05@gmail.com>
2023-04-24 02:48:47 -0400

    Find Student Game

---
+++ src/findStudentGame/FindStudentClass.java
@@ -0,0 +1,81 @@
+package findStudentGame;
+
+//import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class FindStudentClass {
+
+	int hiddenStudentIndex;
+	int wrongGuess;
+	String hiddenStudentName;
+	String[] checkArray = new String[10];
+	String[] nameOfStudents = { "Aleena", "Angel", "Asha", "Clemin", "Gopesh", "Hamneet", "Jimna", "Jobin", "Jude",
+			"Kamaldeep", "Manpreetk", "Manpreets", "Mariya", "Namitha", "Neelam", "Nisha", "Parminder", "Rajat", "Rojy",
+			"Sojan", "Suchitra", "Vindhuja" };
+
+	Scanner sc = new Scanner(System.in);
+
+	String getHiddenStudentName() {
+		Random randomIndex = new Random();
+		hiddenStudentIndex = randomIndex.nextInt(nameOfStudents.length);
+
+		hiddenStudentName = nameOfStudents[hiddenStudentIndex];
+		return hiddenStudentName;
+	}
+
+	void getDashes() {
+		for (int i = 0; i < hiddenStudentName.length(); i++) {
+			System.out.print("_ ");
+		}
+	}
+
+	void getStudentLetter() {
+
+		System.out.println("\nGuess the letter:  ");
+		String letter = sc.nextLine();
+
+		String[] nameArray = hiddenStudentName.split("");
+
+		if (hiddenStudentName.contains(letter)) {
+			System.out.println("You are guessing: ");
+
+			for (int i = 0; i < hiddenStudentName.length(); i++) {
+				if (nameArray[i].equals(letter)) {
+					checkArray[i] = nameArray[i];
+				}
+			}
+		} else {
+			wrongGuess++;
+			System.out.println("\nYou have guessed " + wrongGuess + " wrong letters");
+		}
+		
+		for (int j = 0; j < hiddenStudentName.length(); j++) 
+		{
+			if (checkArray[j] != null) {
+				System.out.print(checkArray[j] + " ");
+								
+			} else {
+				System.out.print("_ ");
+			}
+		}
+		
+		String finalName = String.join("", checkArray);
+		
+		if(finalName.replace("null", "").equals(hiddenStudentName))
+		{
+			System.out.println("\nCongratualtions!!! You have won the game...");
+		}
+		else if(wrongGuess<5)
+		{
+			getStudentLetter();
+		}
+		else
+		{
+			System.out.println("\nSorry!!! You have tried maximum number of wrong attempts");
+		}
+
+	}
+
+	
+}
---
+++ src/findStudentGame/FindStudentMain.java
@@ -0,0 +1,22 @@
+package findStudentGame;
+
+public class FindStudentMain {
+
+	public static void main(String[] args) {
+		
+		FindStudentClass studentGame = new FindStudentClass();
+		
+		String hiddenName = studentGame.getHiddenStudentName();
+		
+		System.out.println("Guess the Student");
+		System.out.println("******************");
+		
+		System.out.println("Student Name: " + hiddenName);
+		
+		System.out.print("Student Name: ");
+		studentGame.getDashes();
+
+		studentGame.getStudentLetter();
+	}
+
+}
